f720d07346b8626a8f849b146b11bec9989441af
CRISTIAN QUEROL 3 <epogonzalo26@gmail.com>
2024-12-19 02:28:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.getElementById("cambiarColorBtn").addEventListener("click", function() {
-  document.body.style.backgroundColor = "#ffcccb";
+let colors = ["#FFB6C1", "#98FB98", "#ADD8E6", "#FFD700", "#FF6347"];
+let currentColorIndex = 0;
+
+document.getElementById("colorButton").addEventListener("click", function() {
+  document.body.style.backgroundColor = colors[currentColorIndex];
+  
+  currentColorIndex++;
+  if (currentColorIndex >= colors.length) {
+    currentColorIndex = 0;
+  }
 });
